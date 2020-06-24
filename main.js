be0836431e1980e605f7a9194c86e5d4edebdf5a
Calo Corp <marc.calo@codeimmersives.com>
2020-06-24 11:30:08 -0400

    done with 15

--- main.js
+++ main.js
@@ -262,3 +262,41 @@ if (count >= num){
  }
  }
  printStringBackwards('radar')
+
+ challengeNumber(15)
+
+ function fizzBuzz(num){
+
+    let count =1
+
+    while(count <=num ){
+
+        
+
+
+        if (count% 3 === 0 && count % 5 !== 0){
+        console.log ("Fizz")}
+
+        else if (count % 5 === 0 && count %3 !== 0){
+
+            console.log ("Buzz")}
+
+        else if (count % 3 === 0 && count % 5 === 0){
+            console.log ("FizzBuzz")
+        }  
+       else console.log(count)
+        count ++
+    } 
+ }
+
+ fizzBuzz(15)
+ 
+ 
+ challengeNumber(16)
+
+ function fibonacci(num){
+
+    let start =1 
+
+
+ }
